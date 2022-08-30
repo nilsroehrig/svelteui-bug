49ab2d7420738b41d840598a68eac3d7fff9a3d4
Nils Röhrig <1717688+nilsroehrig@users.noreply.github.com>
2022-08-30 09:30:07 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import {SimpleGrid} from '@svelteuidev/core'
+  const fruits = ["apple", "banana", "orange", "lemon"];
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<SimpleGrid cols={2}>
+  {#each fruits as fruit}
+    <div>{fruit}</div>
+  {/each}
+</SimpleGrid>
